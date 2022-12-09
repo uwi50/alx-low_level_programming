33ea58a9ead605f890d23a5c7ff7d7f3c5b04185
elvis <uelvis31@gmail.com>
2022-12-09 12:55:33 -0800

    we use if else to determine if the number n is positive ,negative or zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,11 +12,11 @@ int main(void)
 		n = rand() - RAND_MAX / 2;
 		/* your code goes there */
 		printf("%d is ", n);
-		if(n>0)
+		if (n > 0)
 		{
 			printf("positive\n");
 		}
-		else if (n<0)
+		else if (n < 0)
 		{
 			printf("negative\n");
 		}

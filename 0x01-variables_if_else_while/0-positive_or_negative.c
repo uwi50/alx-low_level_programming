c256411e44488366535b89bfb3fc10ddd186e9d8
elvis <uelvis31@gmail.com>
2022-12-09 13:05:41 -0800

    we use if else to determine if the number n is positive ,negative or zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,6 @@
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
-
 /* betty style doc for function main goes there */
 /**
  * main - Entry point
@@ -12,7 +11,6 @@
 int main(void)
 {
 		int n;
-
 		srand(time(0));
 		n = rand() - RAND_MAX / 2;
 		/* your code goes there */

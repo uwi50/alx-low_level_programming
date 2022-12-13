518cc10277ccb9a86cf755d06889740e92e1c9c0
elvis <uelvis31@gmail.com>
2022-12-13 06:02:32 -0800

    program that prints _putchar

---
+++ 0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints out the word "_putchar"
+ *
+ * return: Always 0 (success)
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return 0;
+}

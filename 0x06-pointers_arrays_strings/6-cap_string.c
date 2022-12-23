6fb11de3c310341b92cc7675a42ff64fa16af018
elvis <uelvis31@gmail.com>
2022-12-23 15:01:45 -0800

    a function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,7 +18,7 @@ char *cap_string(char *str)
 		if (str[i - 1] == ' ' || str[i - 1] == '\t' ||
 		str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';'
 		|| str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?'
-		|| str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '\"'
+		|| str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '"'
 		|| str[i - 1] == '{' || str[i - 1] == '}')
 		{
 			str[i] -= 32;

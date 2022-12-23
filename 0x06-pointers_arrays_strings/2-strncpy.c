d723f5eb685ee9883b8cb046f9010661564c0314
elvis <uelvis31@gmail.com>
2022-12-22 19:36:48 -0800

    a function that copies a string for a specified n bytes

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies an inputted number of
+ * bytes (n)
+ * @dest : the buffer storing the string copy
+ * @src: the source string
+ * @n: the maximum number of bytes to copied from src
+ * Return: return the dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, src_len = 0;
+
+	while (src[i++])
+		src_len++;
+	for (i = 0; src[i] && i < n; i++)
+		dest[i] = src[i];
+	for (i = src_len; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}

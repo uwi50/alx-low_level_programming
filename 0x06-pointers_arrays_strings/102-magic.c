dbd865ad638c75f0f7a84319f64dacc197b4103e
elvis <uelvis31@gmail.com>
2022-12-23 16:34:55 -0800

    more on dereferencing

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,8 +1,4 @@
 #include <stdio.h>
-/**
- * main - checks code
- * Return: always 0 (Success)
- */
 
 int main(void)
 {
@@ -20,7 +16,7 @@ int main(void)
 	* - only one statement
 	* - you are not allowed to code anything else than this line of code
 	*/
-	*(p + 5) = 98;
+	*p = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);

fb949084332309ca71a58c6d19baba28a1125d9c
elvis <uelvis31@gmail.com>
2023-01-04 13:52:39 -0800

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- *
  * _strspn - Entry point
  * @s: input
  * @accept: input
@@ -11,16 +10,17 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int n = 0;
 	int r;
+
 	while (*s)
 	{
 		for (r = 0; accept[r]; r++)
 		{
-			if(*s == accept[r])
+			if (*s == accept[r])
 			{
 				n++;
 				break;
 			}
-			else if(accept[r + 1] == '\0')
+			else if (accept[r + 1] == '\0')
 				return (n);
 		}
 		s++;

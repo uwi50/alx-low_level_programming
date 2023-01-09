e9605a4a641ce8ddf22011f0029f7d9c0fbfb28b
elvis <uelvis31@gmail.com>
2023-01-09 04:15:32 -0800

    ex

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - check code
+ * Return: always 0.
+ */
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to be the best hacker\"");
+	_puts("\n\t- Kevin Mitnick");
+	return (0);
+}

bb51aa19ca3eec7dc9cadc9abd819f1ec88bc72c
elvis <uelvis31@gmail.com>
2023-01-18 13:37:40 -0800

    creating dog.h with a struct defined

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This Repo will contain some basic aspects and examples of structures and how typedef is used
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,11 @@
+#ifndef DOG_H
+#define DOG_H
+
+typedef struct dog dog dog
+
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+}
+#endif

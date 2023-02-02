9cdaf3dc26ca216d716f7e008f8cd8bfe7337e64
elvis <uelvis31@gmail.com>
2023-02-02 07:24:33 -0800

    update

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 /**
- *  * add_nodeint_end - adds a new node at the end of a listint_t list
- *   * @head: pointer to head of list
- *    * @n: value of the new node
- *     *
- *      * Return: address of the new element, or NULL if it failed
- *       */
+ * add_nodeint_end - adds a new node at the end of a listint_t list
+ * @head: pointer to head of list
+ * @n: value of the new node
+ *
+ * Return: address of the new element, or NULL if it failed
+ */
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
-/*
- * free_listint - Frees a listint_t list.
+/**
+ * free_listint - frees a listint_t list
  * @head: pointer to a listint_t list
  */
 
